146d89248c7da4d458b1f4a0ccb3f49f1d0344ea
luan <luan.fortunato@warren.com.br>
2022-11-14 09:59:46 -0300

    feat: ceating base for constumer repository

---
+++ Data/Interfaces/ICostumerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    internal interface ICostumerRepository
+    {
+        void Create(Costumer costumer);
+        bool Update(Costumer costumer);
+        bool Delete(long  id);
+        List<Costumer> GetAll();
+        Costumer GetById(int id);
+    }
+}
---
+++ Data/Repositories/CostumerRepository.cs
@@ -0,0 +1,38 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositories
+{
+    internal class CostumerRepository : ICostumerRepository
+    {
+        public void Create(Costumer costumer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Costumer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Costumer GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Costumer costumer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
