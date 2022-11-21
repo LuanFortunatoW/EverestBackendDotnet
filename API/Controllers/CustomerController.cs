1eeaf293d2ff4c0aed91b4385b16e4297e2f63f0
luan <luan.fortunato@warren.com.br>
2022-11-21 09:10:08 -0300

    feat: aplying sugestions

--- API/Controllers/CustomerController.cs
+++ API/Controllers/CustomerController.cs
@@ -60,7 +60,7 @@ namespace API.Controllers
             }
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public virtual IActionResult Delete(long id)
         {
             try
